5d45b3d5563816d61638eb935b5b0372be62dafe
niketan-byte <54742264+niketan-byte@users.noreply.github.com>
2021-09-30 21:08:17 +0530

    Create Knapsack.cpp
    Add another code implementation

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -90,3 +90,65 @@ int main()
 
 	return 0;
 }
+
+#-----------------------------------------------------------------------------------------------------------
+// 0-1 me ya to pura lete he ya pura nhi lete
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e3+2, MOD = 1e9+7;
+
+int val[N],wt[N];
+int dp[N][N];
+
+int knapsack(int n,int w)
+{
+    if(w<=0)
+    {
+        return 0;
+    }
+    if(n<=0)
+    {
+        return 0;
+    }
+    if(dp[n][w]!=-1)
+    {
+        return dp[n][w];
+    }
+    if(wt[n-1]>w)
+    {
+        dp[n][w] = knapsack(n-1,w);
+    }
+     else{
+     dp[n][w] = max(knapsack(n-1,w),knapsack(n-1,w-wt[n-1]) + val[n-1]);
+     }
+  return dp[n][w];
+}
+int main()
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            dp[i][j]=-1;
+        }
+    }
+    int n;
+    cin>>n;
+  
+  for(int i=0;i<n;i++)
+  {
+      cin>>wt[i];
+  }
+
+  for(int i=0;i<n;i++)
+{
+    cin>>val[i];
+}
+int w;
+cin>>w;
+
+cout<<knapsack(n,w);
+
+    return 0;
+}
